f1517c417436263039c60a1187392fc2a4a36fe4
david7482 <david74.chou@gmail.com>
2016-10-03 01:01:57 +0800

    Add main.go

---
+++ src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	gw "example"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:9090", "endpoint of YourService")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterEchoServiceHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	http.ListenAndServe(":8080", mux)
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	if err := run(); err != nil {
+		fmt.Errorf("%v", err)
+	}
+}
